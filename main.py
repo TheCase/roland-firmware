0d6ca10153d000490fd5990169ef8512667889e1
TheCase <case@repulsor.net>
2024-10-20 16:26:09 -0600

    reformat

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from roland_firmware.render import create_html
 
 
 if __name__ == "__main__":
-  models = os.getenv('MODELS',"juno-x").split(',')
-  create_html(models,'content/index.html')
+    models = os.getenv('MODELS', "juno-x").split(',')
+    create_html(models, 'content/index.html')
--- roland_firmware/parse.py
+++ roland_firmware/parse.py
@@ -3,47 +3,52 @@ import urllib.request
 import re
 
 import logging
-fmt="%(asctime)s - %(levelname)-s - %(message)s"
+fmt = "%(asctime)s - %(levelname)-s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=fmt)
 log = logging.getLogger(__name__)
 
+
 def get_content(url):
-  try:
-    html_page = urllib.request.urlopen(url)
-  except Exception as e:
-    raise SystemExit(e)
-  try:
-    soup = BeautifulSoup(html_page, "html.parser")
-  except Exception as e:
-    raise SystemExit(e)
-  return soup
+    try:
+        html_page = urllib.request.urlopen(url)
+    except Exception as e:
+        raise SystemExit(e)
+    try:
+        soup = BeautifulSoup(html_page, "html.parser")
+    except Exception as e:
+        raise SystemExit(e)
+    return soup
+
 
 def firmware_link(model, url):
-  soup = get_content(url)
-  drivers = soup.find("section", {"id": "dl-drivers"})
-  if not drivers:
-    log.error(f"No drivers section found for: '{model}' url: {url}")
-    raise SystemExit("Abort")
-  for item in drivers.findAll('h5'):
-    for link in item.findAll('a'):
-      if 'System Program' in link.string:
-        return link.get('href')
+    soup = get_content(url)
+    drivers = soup.find("section", {"id": "dl-drivers"})
+    if not drivers:
+        log.error(f"No drivers section found for: '{model}' url: {url}")
+        raise SystemExit("Abort")
+    for item in drivers.findAll('h5'):
+        for link in item.findAll('a'):
+            if 'System Program' in link.string:
+                return link.get('href')
+
 
 def firmware_info(url):
-  soup = get_content(url)
-  content = soup.text
-  for line in content.split('\n'):
-    log.debug(line)
-    regex = r"^\s*?\[\s?Ver\.([0-9\.]+)\s?\]\s?(.*)$"
-    match = re.match(regex, line)
-    if match:
-      return url, match
-  log.error("no match found.  Abort.")
-  exit
+    soup = get_content(url)
+    content = soup.text
+    for line in content.split('\n'):
+        log.debug(line)
+        regex = r"^\s*?\[\s?Ver\.([0-9\.]+)\s?\]\s?(.*)$"
+        match = re.match(regex, line)
+        if match:
+            return url, match
+    log.error("no match found.  Abort.")
+    exit
+
 
 def get_latest(model):
-  support_base = "https://www.roland.com/global/support/by_product"
-  support_url  = "{0}/{1}/updates_drivers".format(support_base,model)
-  firmware_url = "https://www.roland.com{0}".format(firmware_link(model, support_url))
-  log.info("{} fw url: {}".format(model, firmware_url))
-  return firmware_info(firmware_url)
+    support_base = "https://www.roland.com/global/support/by_product"
+    support_url = "{0}/{1}/updates_drivers".format(support_base, model)
+    firmware_url = "https://www.roland.com{0}".format(
+        firmware_link(model, support_url))
+    log.info("{} fw url: {}".format(model, firmware_url))
+    return firmware_info(firmware_url)
